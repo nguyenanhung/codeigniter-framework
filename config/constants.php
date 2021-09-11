36248af5cfbd8349130f90e42e980bf78f6f1213
nguyenanhung <info@nguyenanhung.com>
2021-09-11 14:53:57 +0700

    fix constants

--- config/constants.php
+++ config/constants.php
@@ -8,10 +8,12 @@
  * Time: 14:37
  */
 defined('BASEPATH') or exit('No direct script access allowed');
-defined('POWERED_HUNGNG_WEBSITE') or define('HUNGNG_WEBSITE', 'https://nguyenanhung.com');
+defined('HUNGNG_WEBSITE') or define('HUNGNG_WEBSITE', 'https://nguyenanhung.com');
+defined('HUNGNG_EMAIL') or define('HUNGNG_EMAIL', 'dev@nguyenanhung.com');
+defined('POWERED_HUNGNG_WEBSITE') or define('POWERED_HUNGNG_WEBSITE', 'https://nguyenanhung.com');
 defined('POWERED_HUNGNG_BLOG') or define('POWERED_HUNGNG_BLOG', 'https://blog.nguyenanhung.com');
 defined('POWERED_HUNGNG_EMAIL') or define('POWERED_HUNGNG_EMAIL', 'dev@nguyenanhung.com');
-defined('CODEIGNITER_HUNGNG_VENDOR_PACKAGES') or define('CODEIGNITER_HUNGNG_VENDOR_PACKAGES', 'https://packagist.org/packages/nguyenanhung/codeigniter-framework');
-defined('CODEIGNITER_HUNGNG_VENDOR_SOURCE') or define('CODEIGNITER_HUNGNG_VENDOR_SOURCE', 'https://github.com/nguyenanhung/codeigniter-framework');
 defined('POWERED_HUNGNG_NAME') or define('POWERED_HUNGNG_NAME', 'Nguyen An Hung');
 defined('POWERED_HUNGNG_SHORT_NAME') or define('POWERED_HUNGNG_SHORT_NAME', 'Hung Nguyen');
+defined('CODEIGNITER_HUNGNG_VENDOR_PACKAGES') or define('CODEIGNITER_HUNGNG_VENDOR_PACKAGES', 'https://packagist.org/packages/nguyenanhung/codeigniter-framework');
+defined('CODEIGNITER_HUNGNG_VENDOR_SOURCE') or define('CODEIGNITER_HUNGNG_VENDOR_SOURCE', 'https://github.com/nguyenanhung/codeigniter-framework');
