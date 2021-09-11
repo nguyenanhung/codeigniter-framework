c201e3775650024e69c08e271c8346d0a346aa32
nguyenanhung <info@nguyenanhung.com>
2021-09-11 14:43:26 +0700

    Deploy

---
+++ config/constants.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Project codeigniter-framework
+ * Created by PhpStorm
+ * User: 713uk13m <dev@nguyenanhung.com>
+ * Copyright: 713uk13m <dev@nguyenanhung.com>
+ * Date: 09/11/2021
+ * Time: 14:37
+ */
+defined('BASEPATH') or exit('No direct script access allowed');
+defined('POWERED_HUNGNG_WEBSITE') or define('HUNGNG_WEBSITE', 'https://nguyenanhung.com');
+defined('POWERED_HUNGNG_BLOG') or define('POWERED_HUNGNG_BLOG', 'https://blog.nguyenanhung.com');
+defined('POWERED_HUNGNG_EMAIL') or define('POWERED_HUNGNG_EMAIL', 'dev@nguyenanhung.com');
+defined('CODEIGNITER_HUNGNG_VENDOR_PACKAGES') or define('CODEIGNITER_HUNGNG_VENDOR_PACKAGES', 'https://packagist.org/packages/nguyenanhung/codeigniter-framework');
+defined('CODEIGNITER_HUNGNG_VENDOR_SOURCE') or define('CODEIGNITER_HUNGNG_VENDOR_SOURCE', 'https://github.com/nguyenanhung/codeigniter-framework');
+defined('POWERED_HUNGNG_NAME') or define('POWERED_HUNGNG_NAME', 'Nguyen An Hung');
+defined('POWERED_HUNGNG_SHORT_NAME') or define('POWERED_HUNGNG_SHORT_NAME', 'Hung Nguyen');
