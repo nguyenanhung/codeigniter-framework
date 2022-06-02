6ea54d0f05d8fdcd5c034ad21e07dd6f5205e50d
nguyenanhung <info@nguyenanhung.com>
2022-06-03 00:12:31 +0700

    Upgrade CodeIgniter framework version 3.1.13

--- system/helpers/smiley_helper.php
+++ system/helpers/smiley_helper.php
@@ -6,7 +6,7 @@
  *
  * This content is released under the MIT License (MIT)
  *
- * Copyright (c) 2014 - 2019, British Columbia Institute of Technology
+ * Copyright (c) 2019 - 2022, CodeIgniter Foundation
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -29,8 +29,9 @@
  * @package	CodeIgniter
  * @author	EllisLab Dev Team
  * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
- * @copyright	Copyright (c) 2014 - 2019, British Columbia Institute of Technology (http://bcit.ca/)
- * @license	http://opensource.org/licenses/MIT	MIT License
+ * @copyright	Copyright (c) 2014 - 2019, British Columbia Institute of Technology (https://bcit.ca/)
+ * @copyright	Copyright (c) 2019 - 2022, CodeIgniter Foundation (https://codeigniter.com/)
+ * @license	https://opensource.org/licenses/MIT	MIT License
  * @link	https://codeigniter.com
  * @since	Version 1.0.0
  * @filesource
@@ -239,6 +240,7 @@ if ( ! function_exists('_get_smiley_array'))
 			{
 				include(APPPATH.'config/smileys.php');
 			}
+
 			if (file_exists(APPPATH.'config/'.ENVIRONMENT.'/smileys.php'))
 			{
 				include(APPPATH.'config/'.ENVIRONMENT.'/smileys.php');
