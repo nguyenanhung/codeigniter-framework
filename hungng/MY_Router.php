41efe3369c6370e0609ddb08a79c240fa2109193
nguyenanhung <info@nguyenanhung.com>
2022-07-30 00:41:00 +0700

    Add my router and my loader

---
+++ hungng/MY_Router.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Project codeigniter-framework
+ * Created by PhpStorm
+ * User: 713uk13m <dev@nguyenanhung.com>
+ * Copyright: 713uk13m <dev@nguyenanhung.com>
+ * Date: 30/07/2022
+ * Time: 00:39
+ */
+if (!class_exists('MY_Router')) {
+	class MY_Router extends HungNG_Router
+	{
+
+	}
+}
