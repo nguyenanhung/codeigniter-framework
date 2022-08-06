43240dd0669ab116e87f75811bf2afd0d98d3922
nguyenanhung <info@nguyenanhung.com>
2022-08-06 23:49:11 +0700

    Deploy

---
+++ hungng/BEAR_Lang.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Project codeigniter-framework
+ * Created by PhpStorm
+ * User: 713uk13m <dev@nguyenanhung.com>
+ * Copyright: 713uk13m <dev@nguyenanhung.com>
+ * Date: 30/07/2022
+ * Time: 00:38
+ */
+if (!class_exists('BEAR_Lang')) {
+	class BEAR_Lang extends HungNG_Lang
+	{
+	}
+}
---
+++ hungng/BEAR_Model.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Project codeigniter-framework
+ * Created by PhpStorm
+ * User: 713uk13m <dev@nguyenanhung.com>
+ * Copyright: 713uk13m <dev@nguyenanhung.com>
+ * Date: 30/07/2022
+ * Time: 00:38
+ */
+if (!class_exists('BEAR_Model')) {
+	class BEAR_Model extends HungNG_Model
+	{
+	}
+}
