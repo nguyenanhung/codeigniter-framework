09345f1e33fe88f60dce591ec5b4090a56488a5f
nguyenanhung <info@nguyenanhung.com>
2023-04-11 09:04:27 +0700

    Add .phpstorm.php IDE

---
+++ .phpstorm.php
@@ -0,0 +1,8 @@
+<?php
+die('This file is used for development purposes only.');
+const ENVIRONMENT = '';
+const APPPATH = '';
+const SYSDIR = '';
+const FCPATH = '';
+const BASEPATH = '';
+const VIEWPATH = '';
