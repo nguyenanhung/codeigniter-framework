0f396642dcbcdbad1d5e4b53f678e924c7eeedc3
nguyenanhung <info@nguyenanhung.com>
2023-04-23 23:24:57 +0700

    Update Project at Sun Apr 23 23:24:57 +07 2023

--- system/helpers/seo_helper.php
+++ system/helpers/seo_helper.php
@@ -8,16 +8,16 @@ if (!function_exists('bear_simple_seo_header')) {
 	 * @param $description
 	 * @param $image
 	 *
-	 * @return mixed
 	 * @author   : 713uk13m <dev@nguyenanhung.com>
 	 * @copyright: 713uk13m <dev@nguyenanhung.com>
-	 * @time     : 23/04/2023 22:17
+	 * @time     : 23/04/2023 23:49
 	 */
 	function bear_simple_seo_header($title = '', $description = '', $image = '')
 	{
 		$CI =& get_instance();
 		$cms = $CI->load->library('seo_onpage');
-
-		return $cms->seo_onpage->published($title, $description, $image);
+		/** @var \CI_Seo_onpage $seo */
+		$seo = $cms->seo_onpage;
+		$seo->published($title, $description, $image);
 	}
 }
